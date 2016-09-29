6ea5a82c831dd1a613a8f612a65fd7a6d4a8f91d
BrianFlannery <bflannery.0wK0@icloud.com>
2016-09-28 20:33:01 -0600

    Update theme1.js

--- theme1.js
+++ theme1.js
@@ -1,10 +1,13 @@
 
 document.observe("dom:loaded", function () {
     if ( ! $$('div.classificationBanner')[0] ) {
+      var headerBannerW = new Element('div');
+      headerBanner.className = 'classificationBannerW';
       var headerBanner = new Element('div');
+      headerBannerW.insert(headerBanner);
       headerBanner.className = 'classificationBanner';
       headerBanner.innerHTML = 'UNCLASSIFIED';
-      $$('body')[0].insertBefore(headerBanner, $$('div#page-head')[0]);
+      $$('body')[0].insertBefore(headerBannerW, $$('div#page-head')[0]);
     }
 });
 
