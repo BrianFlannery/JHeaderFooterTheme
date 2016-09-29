cbd9fa70f2cabb36206f2266de904b0e5ad8d3c3
BrianFlannery <bflannery.0wK0@icloud.com>
2016-09-28 20:24:22 -0600

    Update theme1.js

--- theme1.js
+++ theme1.js
@@ -3,7 +3,7 @@ document.observe("dom:loaded", function () {
     if ( ! $$('div.classificationBanner')[0] ) {
       var headerBanner = new Element('div');
       headerBanner.className = 'classificationBanner';
-      $$('body')[0].insertBefore(headerBanner, $$('div#page-head'));
+      $$('body')[0].insertBefore(headerBanner, $$('div#page-head')[0]);
     }
 });
 
