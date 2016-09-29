a2751baa0d2e5d82644c6ceca8ae8953ea1cce0a
BrianFlannery <bflannery.0wK0@icloud.com>
2016-09-28 20:45:59 -0600

    Update theme1.js

--- theme1.js
+++ theme1.js
@@ -2,12 +2,14 @@
 document.observe("dom:loaded", function () {
     if ( ! $$('div.classificationBanner')[0] ) {
       var headerBannerW = new Element('div');
-      headerBanner.className = 'classificationBannerW';
+      headerBannerW.className = 'classificationBannerW';
+        
       var headerBanner = new Element('div');
       headerBanner.innerHTML = 'UNCLASSIFIED';
       headerBanner.className = 'classification-banner';
       headerBanner.className += ' unclassified';
       headerBanner.className += ' vertal';
+        
       headerBannerW.className += ' vertal';
       headerBannerW.insert(headerBanner);
       $$('body')[0].insertBefore(headerBannerW, $$('div#page-head')[0]);
