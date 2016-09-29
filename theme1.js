0e47ebbc97ddd49e4a89bb362f2937ae744da054
BrianFlannery <bflannery.0wK0@icloud.com>
2016-09-28 20:12:51 -0600

    Create theme1.js

---
+++ theme1.js
@@ -0,0 +1,10 @@
+
+document.observe("dom:loaded", function () {
+    if ( ! $$('div.classificationBanner')[0] ) {
+      var headerBanner = new Element('div');
+      headerBanner.className = 'classificationBanner';
+      $$('body')[0].insertBefore(headerBanner, $$('div#page-head'));
+    }
+});
+
+//
